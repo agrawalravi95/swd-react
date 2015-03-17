23de94e36a7b2de7119631332c9eaf30a7a3f358
Nihanth Subramanya <nhnt11@gmail.com>
2015-03-17 23:41:37 +0530

    DB API: Initial hello world

---
+++ src/stores/DBStore.js
@@ -0,0 +1,18 @@
+/*
+ * React.js Starter Kit
+ * Copyright (c) 2014 Konstantin Tarkus (@koistya), KriaSoft LLC.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.txt file in the root directory of this source tree.
+ */
+
+'use strict';
+
+var DBStore = {
+  process: function(req) {
+    return "Hello, world!";
+  }
+};
+
+module.exports = DBStore;
+
